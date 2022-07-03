0cdba24727b1fcc96728142553098221d3ed0601
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-03 04:45:03 +0300

    Add header to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import Header from "@/components/Header";
 
 const Home: NextPage = () => {
-  return <>hello</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default Home;
