2a741a9c2789e9fb4efc71c645726e297138e585
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-03 23:04:50 +0300

    Install and set up swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { SWRConfig } from "swr";
 import "../styles/globals.css";
 
 const darkTheme = createTheme({
@@ -11,11 +12,18 @@ const darkTheme = createTheme({
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider theme={darkTheme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+      }}
+    >
+      <SessionProvider session={session}>
+        <ThemeProvider theme={darkTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </SWRConfig>
   );
 }
 
