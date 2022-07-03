d14238b1fd97f4807f4f069b1ac950a4928ec969
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-03 04:43:51 +0300

    Set up material ui with dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "../styles/globals.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
