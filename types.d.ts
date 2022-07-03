43aba35fda6bc44ecf3e5c16c8aef500710b8845
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-03 23:00:41 +0300

    Use custom user typing for next-auth session user

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+import { DefaultUser } from "next-auth";
+import { SessionUser } from "@/types/User";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: SessionUser;
+  }
+}
