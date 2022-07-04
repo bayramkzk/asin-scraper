06796fc087bdf7e9574bf2392d56185aca0731e7
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-04 17:40:07 +0300

    Configure auth protection middleware to exclude login page

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,1 +1,19 @@
-export { default } from "next-auth/middleware";
+import type { NextRequest } from "next/server";
+import type { JWT } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  // @ts-ignore: Next-auth typing problems
+  function middleware(req: NextRequest & { nextauth: { token: JWT | null } }) {
+    console.log(req.nextauth.token);
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        const authorized =
+          !!token?.email || req.nextUrl.pathname === "/auth/signin";
+        return authorized;
+      },
+    },
+  }
+);
