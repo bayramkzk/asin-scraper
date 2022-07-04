7fd79f4bf18dc9113ccd4f15261c00874b933766
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-04 07:13:26 +0300

    Add ProductDataGrid to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import Header from "@/components/Header";
+import ProductDataGrid from "@/components/ProductDataGrid";
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
+      <ProductDataGrid />
     </>
   );
 };
