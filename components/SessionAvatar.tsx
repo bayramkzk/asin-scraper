4790352a6d4f9589611b734c25fad47f60dc38f6
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-04 15:07:54 +0300

    Add size and fontSize props to SessionAvatar

--- components/SessionAvatar.tsx
+++ components/SessionAvatar.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { useSession } from "next-auth/react";
 import LetterAvatar from "./LetterAvatar";
+import WarningIcon from "@mui/icons-material/Warning";
 import { SessionUser } from "@/types/User";
 
-interface SessionAvatarProps {}
+interface SessionAvatarProps {
+  size?: number | string;
+  fontSize?: number | string;
+}
 
-const SessionAvatar: React.FC<SessionAvatarProps> = (props) => {
+const SessionAvatar: React.FC<SessionAvatarProps> = ({ size, fontSize }) => {
   const { data: session, status } = useSession();
 
   if (status === "authenticated") {
     const user = session.user as SessionUser;
-    return <LetterAvatar name={user.name} />;
+    return (
+      <LetterAvatar
+        name={user.name}
+        sx={{ width: size, height: size, fontSize }}
+      />
+    );
   }
 
-  return <a href="/api/auth/signin">Sign in</a>;
+  return <WarningIcon />;
 };
 
 export default SessionAvatar;
