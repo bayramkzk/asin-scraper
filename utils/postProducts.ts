89ec1b5b8d4fe3c83badb950fed1336f936aac61
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-04 07:10:42 +0300

    Create postProducts.ts

---
+++ utils/postProducts.ts
@@ -0,0 +1,52 @@
+import prisma from "@/lib/prisma";
+import ProductData from "@/types/Product";
+import ApiContext from "@/types/ApiContext";
+import ProductContext from "@/types/ProductContext";
+import parseProductHtml from "./parseProduct";
+
+export interface PostProductsResult {
+  data: PromiseSettledResult<ProductData>[];
+  createdCount: number;
+}
+
+export async function fetchProductContext(
+  asin: string
+): Promise<ProductContext> {
+  const [htmlCom, htmlAe, htmlAeDollar] = await Promise.all([
+    fetch(`https://www.amazon.com/dp/${asin}`, {
+      headers: { Cookie: process.env.AMAZON_COM_COOKIE as string },
+    }).then((res) => res.text()),
+    fetch(`https://www.amazon.ae/dp/${asin}?language=en_AE&currency=AED`).then(
+      (res) => res.text()
+    ),
+    fetch(`https://www.amazon.ae/dp/${asin}?language=en_AE&currency=USD`, {
+      headers: { Cookie: process.env.AMAZON_AE_DOLLAR_COOKIE as string },
+    }).then((res) => res.text()),
+  ]);
+
+  return { asin, htmlCom, htmlAe, htmlAeDollar };
+}
+
+export default async function postProducts({ req, res, user }: ApiContext) {
+  if (!req.body.asinCodes) {
+    return res.status(400).json({ error: "Missing required body field" });
+  }
+
+  const asinCodes = req.body.asinCodes as string[];
+
+  const settledProducts = await Promise.allSettled(
+    asinCodes.map((asin) => fetchProductContext(asin).then(parseProductHtml))
+  );
+
+  const fulfilledProducts = settledProducts
+    .filter((r) => r.status === "fulfilled")
+    .map((r) => r as PromiseFulfilledResult<ProductData>)
+    .map((r) => r.value);
+
+  const { count: createdCount } = await prisma.product.createMany({
+    data: fulfilledProducts.map((p) => ({ ...p, authorId: user.id })),
+    skipDuplicates: true,
+  });
+
+  res.status(200).json({ data: settledProducts, createdCount });
+}
