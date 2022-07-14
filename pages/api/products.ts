90e64d5df0b44e4ec02332c7e9a37dc7df808233
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-15 00:08:05 +0300

    Remove user from ApiContext in api/products

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -33,7 +33,7 @@ export default async function handler(
     return res.status(401).json({ error: "User not found" });
   }
 
-  const context: ApiContext = { req, res, user, session };
+  const context: ApiContext = { req, res, session };
 
   switch (req.method) {
     case "POST":
