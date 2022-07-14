6f907d9b949c84de3f2080eecbef46b3fd68ed66
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-15 00:05:28 +0300

    Create users page with UserDataGrid

---
+++ components/UserDataGrid.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import LinearProgress from "@mui/material/LinearProgress";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import useUsers from "@/hooks/useUsers";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", type: "number", width: 100 },
+  { field: "name", headerName: "Name", type: "string", width: 200 },
+  { field: "email", headerName: "Email", type: "string", width: 250 },
+  { field: "createdAt", headerName: "Created At", type: "date", width: 250 },
+  { field: "role", headerName: "Role", type: "role", width: 100 },
+];
+
+export default function UserDataGrid() {
+  const { loading, error, users } = useUsers();
+
+  if (loading) {
+    return <LinearProgress />;
+  }
+
+  if (error || users === undefined) {
+    return (
+      <Container maxWidth="xl" sx={{ height: "100%", paddingBlock: "1rem" }}>
+        <Alert severity="error">{error.info ?? "An error occurred!"}</Alert>
+      </Container>
+    );
+  }
+
+  return (
+    <Container maxWidth="xl" sx={{ height: "100%", paddingBlock: "1rem" }}>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+        }}
+      >
+        <DataGrid
+          rows={users}
+          columns={columns}
+          checkboxSelection
+          disableSelectionOnClick
+        />
+      </Box>
+    </Container>
+  );
+}
---
+++ pages/users.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import UserDataGrid from "@/components/UserDataGrid";
+
+export default function UsersPage() {
+  return (
+    <>
+      <Header />
+      <UserDataGrid />
+    </>
+  );
+}
