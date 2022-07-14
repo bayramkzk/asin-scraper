37406f27cdc30031a0ba4b1318b8b5324bff8405
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-15 00:02:07 +0300

    Check for ownership of product in deleteProducts

--- utils/deleteProducts.ts
+++ utils/deleteProducts.ts
@@ -1,7 +1,11 @@
 import prisma from "@/lib/prisma";
 import ApiContext from "@/types/ApiContext";
 
-export default async function deleteProducts({ req, res }: ApiContext) {
+export default async function deleteProducts({
+  req,
+  res,
+  session,
+}: ApiContext) {
   if (!req.body.idList) {
     return res.status(400).json({ error: "Missing required body field" });
   }
@@ -9,7 +13,7 @@ export default async function deleteProducts({ req, res }: ApiContext) {
   const idList = req.body.idList as number[];
 
   const result = await prisma.product.deleteMany({
-    where: { id: { in: idList } },
+    where: { id: { in: idList }, authorId: session.user.id },
   });
 
   res.status(200).json(result);
