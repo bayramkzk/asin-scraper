d85ef46f3b7f3c1260791b26ebe58d987d5c9e6f
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-07-15 00:04:44 +0300

    Create API endpoint for users

---
+++ pages/api/users.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import ApiContext from "@/types/ApiContext";
+import postUser from "@/utils/postUser";
+import getUsers from "@/utils/getUsers";
+import patchUser from "@/utils/patchUser";
+import deleteUsers from "@/utils/deleteUsers";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (
+    req.method !== "POST" &&
+    req.method !== "GET" &&
+    req.method !== "PATCH" &&
+    req.method !== "DELETE"
+  ) {
+    return res.status(404).json({ error: "Undefined method" });
+  }
+
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    return res.status(500).json({ error: "No session acquired" });
+  }
+
+  const context: ApiContext = { req, res, session };
+
+  switch (req.method) {
+    case "POST":
+      return await postUser(context);
+    case "GET":
+      return await getUsers(context);
+    case "PATCH":
+      return await patchUser(context);
+    case "DELETE":
+      return await deleteUsers(context);
+  }
+}
