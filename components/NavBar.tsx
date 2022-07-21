54dcbcf912954a95992f411b0e35cf853713062f
jir256 <48856944+bayramdev@users.noreply.github.com>
2022-07-21 14:06:30 +0300

    Remove highlighting of current route in NavBar in light mode

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,6 +3,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Page from "@/types/Page";
 import { useRouter } from "next/router";
+import { useTheme } from "@mui/material/styles";
 
 interface NavBarProps {
   pages: Page[];
@@ -10,6 +11,7 @@ interface NavBarProps {
 
 function NavBar({ pages }: NavBarProps) {
   const router = useRouter();
+  const theme = useTheme();
 
   return (
     <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
@@ -17,8 +19,15 @@ function NavBar({ pages }: NavBarProps) {
         <Button
           key={page.name}
           onClick={() => router.push(page.href)}
-          sx={{ my: 2, display: "block" }}
-          color={router.pathname === page.href ? "primary" : "inherit"}
+          sx={{
+            my: 2,
+            display: "block",
+          }}
+          color={
+            theme.palette.mode === "dark" && router.pathname === page.href
+              ? "primary"
+              : "inherit"
+          }
         >
           {page.name}
         </Button>
