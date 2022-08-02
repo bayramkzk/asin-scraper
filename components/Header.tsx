661c1fbfc8dd5ba1776e052426471a15451392c4
highberg <48856944+highberg@users.noreply.github.com>
2022-08-02 16:09:39 +0300

    Remove unused useRouter() call in Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,6 @@ import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 import LogoLabelDesktop from "./LogoLabelDesktop";
 import LogoLabelMobile from "./LogoLabelMobile";
 import NavBar from "./NavBar";
@@ -11,7 +10,6 @@ import NavMenu from "./NavMenu";
 import ProfileMenu from "./ProfileMenu";
 
 const Header = () => {
-  const router = useRouter();
   const { data: session } = useSession();
   const isAdmin =
     session &&
