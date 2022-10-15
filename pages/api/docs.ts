3c5ec4c2bb9e897b5b9eea3e4a12740fd39ab491
highberg <48856944+highberg@users.noreply.github.com>
2022-10-15 13:29:30 +0300

    Replace reading from filesystem with db in docs api

--- pages/api/docs.ts
+++ pages/api/docs.ts
@@ -1,18 +1,35 @@
-import fs from "fs/promises";
+import { prisma } from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
+
+type Queryparam = string | string[] | undefined;
+type Domain = "htmlCom" | "htmlAe" | "htmlAeDollar";
+
+const checkAsin = (asin: Queryparam) =>
+  typeof asin === "string" && asin.length === 10;
+
+const checkDomain = (domain: Queryparam) =>
+  typeof domain === "string" &&
+  (domain === "htmlCom" || domain === "htmlAe" || domain === "htmlAeDollar");
+
+const validateQuery = (query: NextApiRequest["query"]) => {
+  const { asin, domain } = query;
+  if (checkAsin(asin) && checkDomain(domain)) {
+    return { asin: asin as string, domain: domain as Domain };
+  }
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { asin, domain } = req.query;
-  if (!asin || !domain || Array.isArray(asin) || Array.isArray(domain)) {
-    return res.status(400).json({ error: "Bad Request" });
-  }
-  try {
-    const html = await fs.readFile(`./html/${asin}-${domain}.html`, "utf-8");
-    res.status(200).send(html);
-  } catch (e) {
-    return res.status(404).json({ error: "Not Found" });
-  }
+  const body = validateQuery(req.query);
+  if (req.method !== "GET" || !body) return res.status(400).end();
+
+  const log = await prisma.productHtmlLog.findUnique({
+    where: { asin: body.asin },
+  });
+  if (!log) return res.status(404).json({ error: "Not Found" });
+
+  const doc = log[body.domain];
+  res.send(doc);
 }
